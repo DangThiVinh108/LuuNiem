d4796dbcde8c46bd00200c2846cefe2239aa97b2
DangThiVinh108 <vinhmaria0901@gmail.com>
2023-10-30 11:00:05 +0700

    init project

---
+++ LuuNiem/Login.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LuuNiem
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private MyDb db = new MyDb();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string password = pbPassWord.Password;
+            string query = $"SELECT * FROM tblUsers WHERE UserName = '{userName}' AND Password = '{password}'";
+            SqlDataReader sqlDataReader = db.GetDataReader(query);
+            if (sqlDataReader.HasRows)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu chưa chính xác!");
+            }
+            sqlDataReader.Close();
+        }
+
+        private void chkShow_Checked(object sender, RoutedEventArgs e)
+        {
+            txtPasswordShow.Text = pbPassWord.Password;
+            txtPasswordShow.Visibility = Visibility.Visible;
+            pbPassWord.Visibility = Visibility.Hidden;
+            
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void chkShow_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtPasswordShow.Visibility = Visibility.Hidden;
+            pbPassWord.Visibility = Visibility.Visible;
+            
+
+        }
+
+        private void txtPasswordShow_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            pbPassWord.Password = txtPasswordShow.Text;
+        }
+    }
+}
---
+++ LuuNiem/MyDb.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LuuNiem
+{
+    internal class MyDb
+    {
+        //Khai báo biến
+        private static SqlConnection mySqlConnection;
+
+        public MyDb()
+        {
+            string severName = Environment.MachineName;
+
+            try
+            {
+                mySqlConnection = new SqlConnection($"Data Source ={severName};Initial Catalog=LuuNiem;Integrated Security=True");
+                //conn = new SqlConnection($"Data Source={severName};Initial Catalog=LuuNiem;Integrated Security=False;User ID=UserName;Password=YourPassword;");
+                mySqlConnection.Open();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error" + ex.Number.ToString());
+            }
+        }
+        public DataTable GetDataTable( string sSql)
+        {
+            DataTable myDataTable = new DataTable();
+            try
+            {
+                SqlDataAdapter mySqlDataAdapter = new SqlDataAdapter(sSql, mySqlConnection);
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(mySqlDataAdapter);
+                mySqlDataAdapter.Fill(myDataTable);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error" + ex.Number.ToString());
+                return null;
+            }
+            return myDataTable;
+        }
+        public SqlDataReader GetDataReader(string sQuery)
+        {
+            SqlDataReader dataReader;
+            try
+            {
+                SqlCommand command = new SqlCommand(sQuery, mySqlConnection);
+                dataReader = command.ExecuteReader();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error" + ex.Number.ToString());
+                return null;
+            }
+            return dataReader;
+        }
+        public void RunNonQuery(string sSql)
+        {
+            SqlCommand mySqlCommand = new SqlCommand(sSql, mySqlConnection);
+            try
+            {
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error" + ex.Source.ToString() + ": " + ex.LineNumber.ToString());
+            }
+        }
+        
+    }
+    
+}
